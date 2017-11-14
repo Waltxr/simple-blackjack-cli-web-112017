e8b4f9b0b945ba295843657efe23acc59a62b2af
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 21:12:46 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,9 +53,8 @@ end
 #####################################################
 
 def runner
-  card_total = 0
   welcome
-  initial_round
+  card_total = initial_round
   until card_total > 21
     hit?(card_total)
     display_card_total
